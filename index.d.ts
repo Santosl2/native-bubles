6271f3984fb80140b5cde0b27a6ca8e8ddc636dd
Santosl2 <mfilype2019@gmail.com>
2023-06-19 12:41:51 -0300

    feat: minor adjusts

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 declare module "native-bubbles" {
   export function reopenApp(): void;
-  export function showFloatingBubble(x?: number, y?: number): void;
-  export function hideFloatingBubble(): void;
-  export function checkPermission(): void;
-  export function requestPermission(): void;
-  export function initialize(): void;
+  export function showFloatingBubble(x?: number, y?: number): Promise<void>;
+  export function hideFloatingBubble(): Promise<void>;
+  export function checkPermission(): Promise<void>;
+  export function requestPermission(): Promise<void>;
+  export function initialize(): Promise<void>;
 }
